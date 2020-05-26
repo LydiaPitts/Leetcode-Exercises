3c1c870fd90c17802593520b0c1d252fb919a036
Lydia Pitts <lpitts@bowdoin.edu>
2020-05-26 15:36:36 -0400

    Problem 104, Maximum Depth of Binary Tree in java.

---
+++ Problem104.java
@@ -0,0 +1,46 @@
+/**
+ * Author: Lydia Pitts
+ *
+ * Given a binary tree, find its maximum depth.
+ * The maximum depth is the number of nodes along the longest 
+ * path from the root node down to the farthest leaf node.
+ *
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if(root == null){
+            return 0;
+        }
+        if((root.left == null) & (root.right == null)){
+            return 1;
+        }
+        
+            int leftVal = 1 + maxDepth(root.left);
+            int rightVal = 1 + maxDepth(root.right);
+            if(leftVal >= rightVal){
+                return leftVal;
+            } else {
+                return rightVal;
+        }
+    }
+}
+/**
+ * Runtime: 0 ms, faster than 100.00% of Java online submissions 
+ * for Maximum Depth of Binary Tree.
+ * 
+ * Memory Usage: 39.1 MB, less than 94.62% of Java online submissions 
+ * for Maximum Depth of Binary Tree.
+*/
